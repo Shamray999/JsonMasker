85d76b46a3ebe10a1fe41ca0527ad576fb049999
Shamray <guy.shamray@gmail.com>
2021-03-31 18:11:11 +0300

    Add more unit tests

--- JsonMasker.Tests/JsonMaskerUnitTests.cs
+++ JsonMasker.Tests/JsonMaskerUnitTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace JsonMasker.Tests
@@ -13,31 +14,90 @@ namespace JsonMasker.Tests
             JsonData = @"
             {
               ""Result"": {
-                ""loginData"": {
+                ""UserData"": {
                   ""UserName"": ""TestUser"",
                   ""Password"": ""TestPassword""
                 },
-                ""creditCards"": [
-                                    { ""cardNum"": ""4581111111111"", ""cvv"": ""123"" },
-                                    { ""cardNum"": ""4581111111234"", ""cvv"": ""589"" },
-                                    { ""cardNum"": ""4581111115698"", ""cvv"": ""987"" },
-                                    { ""cardNum"": ""4581111117978"", ""cvv"": ""987"" }
+                ""CreditCards"": [
+                                    { ""CardNum"": ""4581111111111"", ""CVV"": ""123"" },
+                                    { ""CardNum"": ""4581111111234"", ""CVV"": ""589"" },
+                                    { ""CardNum"": ""4581111115698"", ""CVV"": ""987"" },
+                                    { ""CardNum"": ""4581111117978"", ""CVV"": ""987"" }
                                  ]
               }
             }";
         }
 
         [Fact]
-        public void Test1()
+        public void MaskUserNameFull()
         {
             var maskConfig = new List<MaskerConfig>()
             {
-                new MaskerConfig(){ JsonPath = "Result.creditCards[*].cardNum"},
+                new MaskerConfig(){ JsonPath = "Result.UserData.UserName"},
             };
 
             var jsonMasker = new JsonMasker();
             var maskedData = jsonMasker.MaskJson(JsonData, maskConfig);
 
+            dynamic maskedJson = JsonConvert.DeserializeObject(maskedData);
+            string maskedUserName = maskedJson.Result.UserData.UserName;
+
+            Assert.Equal("********", maskedUserName);
+        }
+
+        [Fact]
+        public void MaskUserNamePartial()
+        {
+            var maskConfig = new List<MaskerConfig>()
+            {
+                new MaskerConfig(){ JsonPath = "Result.UserData.UserName", ShowFirst = 2, ShowLast = 2},
+            };
+
+            var jsonMasker = new JsonMasker();
+            var maskedData = jsonMasker.MaskJson(JsonData, maskConfig);
+
+            dynamic maskedJson = JsonConvert.DeserializeObject(maskedData);
+            string maskedUserName = maskedJson.Result.UserData.UserName;
+
+            Assert.Equal("Te****er", maskedUserName);
+        }
+
+        [Fact]
+        public void MaskFullCardNumList()
+        {
+            var maskConfig = new List<MaskerConfig>()
+            {
+                new MaskerConfig(){ JsonPath = "Result.CreditCards[*].CardNum"},
+            };
+
+            var jsonMasker = new JsonMasker();
+            var maskedData = jsonMasker.MaskJson(JsonData, maskConfig);
+            dynamic maskedJson = JsonConvert.DeserializeObject(maskedData);
+
+            foreach (dynamic item in maskedJson.Result.CreditCards)
+            {
+                string cardNum = item.CardNum;
+                Assert.Equal("*************", cardNum);
+            }
+        }
+
+        [Fact]
+        public void MaskPartialCardNumList()
+        {
+            var maskConfig = new List<MaskerConfig>()
+            {
+                new MaskerConfig(){ JsonPath = "$..CardNum", ShowFirst = 4, ShowLast = 4},
+            };
+
+            var jsonMasker = new JsonMasker();
+            var maskedData = jsonMasker.MaskJson(JsonData, maskConfig);
+            dynamic maskedJson = JsonConvert.DeserializeObject(maskedData);
+
+            foreach (dynamic item in maskedJson.Result.CreditCards)
+            {
+                string cardNum = item.CardNum;
+                Assert.Matches(@"\d{4}\*{5}\d{4}$", cardNum);
+            }
         }
     }
 }
